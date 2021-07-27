592ac42a83373b45821eb50caed73970d303b2a1
mario <148373644@qq.com>
2021-07-27 23:24:04 +0800

    页面基本布局

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,7 @@ export default{
 
 </script>
 
+<style lang="scss">
+@import "./assets/style/reset.css";
+@import "./assets/style/index.scss";
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,30 @@
 <template>
-  <div>
-     <h2>home</h2>
-     <router-view></router-view>
+  <div class="basic-layout">
+    <div class="nav-side">
+      <!-- 系统logo -->
+      <div class="logo">
+        <img src="./../assets/logo.png" />
+        <span>Manager</span>
+      </div>
+      <!-- 导航栏 -->
+
+    </div>
+    <div class="content-right">
+      <!-- 头部 -->
+        <div class="nav-top"></div>
+        <!-- 内容部分 -->
+        <div class="wrapper">
+          <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name:"home"
-  }
+export default {
+  name: "home"
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
