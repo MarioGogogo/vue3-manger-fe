78761eb93b157b41239048dbaa057d95f97e3f4d
MarioGogogo <148373644@qq.com>
2021-08-15 21:56:37 +0800

    特性(登录): 增加404页面 2.增加路由守卫判断

--- src/router/index.js
+++ src/router/index.js
@@ -1,73 +1,92 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '@/components/Home.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '@/components/Home.vue';
 
 const routes = [
-    {
-        name:'home',
-        path:'/',
-        meta:{
-            title:'首页'
-        },
-        component:Home,
-        redirect:'/welcome',
-        children:[
-            {
-                name:'welcome',
-                path:'/welcome',
-                meta:{
-                    /*title:'欢迎页'*/
-                    title:'欢迎体验Vue3全栈课程'
-                },
-                component:()=>import('@/views/Welcome.vue')
-            },
-            {
-                name:'user',
-                path:'/system/user',
-                meta:{
-                    title:'用户管理'
-                },
-                component:()=>import('@/views/User.vue')
-            },
-            {
-                name:'menu',
-                path:'/system/menu',
-                meta:{
-                    title:'菜单管理'
-                },
-                component:()=>import('@/views/Menu.vue')
-            },
-            {
-                name:'role',
-                path:'/system/role',
-                meta:{
-                    title:'角色管理'
-                },
-                component:()=>import('@/views/Role.vue')
-            },
-            {
-                name:'dept',
-                path:'/system/dept',
-                meta:{
-                    title:'部门管理'
-                },
-                component:()=>import('@/views/Dept.vue')
-            }
-        ]
+  {
+    name: 'home',
+    path: '/',
+    meta: {
+      title: '首页',
     },
-    {
-        name:'login',
-        path:'/login',
-        meta:{
-            title:'登录'
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      {
+        name: 'welcome',
+        path: '/welcome',
+        meta: {
+          /*title:'欢迎页'*/
+          title: '欢迎体验Vue3全栈课程',
         },
-        component:()=>import('@/views/Login.vue')
-    }
-]
+        component: () => import('@/views/Welcome.vue'),
+      },
+      {
+        name: 'user',
+        path: '/system/user',
+        meta: {
+          title: '用户管理',
+        },
+        component: () => import('@/views/User.vue'),
+      },
+      {
+        name: 'menu',
+        path: '/system/menu',
+        meta: {
+          title: '菜单管理',
+        },
+        component: () => import('@/views/Menu.vue'),
+      },
+      {
+        name: 'role',
+        path: '/system/role',
+        meta: {
+          title: '角色管理',
+        },
+        component: () => import('@/views/Role.vue'),
+      },
+      {
+        name: 'dept',
+        path: '/system/dept',
+        meta: {
+          title: '部门管理',
+        },
+        component: () => import('@/views/Dept.vue'),
+      },
+    ],
+  },
+  {
+    name: 'login',
+    path: '/login',
+    meta: {
+      title: '登录',
+    },
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    name: '404',
+    path: '/404',
+    meta: {
+      title: '页面不存在',
+    },
+    component: () => import('@/views/404.vue'),
+  },
+];
 const router = createRouter({
-    history:createWebHashHistory(),
-    routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 
-export default router
+// 导航守卫
+router.beforeEach((to, from, next) => {
+  //判断当前路由是否存在
+  if (router.hasRoute(to.name)) {
+    document.title = to.meta.title;
+    next();
+  } else {
+    next('/404');
+  }
+});
+
+export default router;
 
 // TODO 权限需要导航守卫
---
+++ src/views/404.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="notFind">
+    <img src="@/assets/images/404.gif" alt="" />
+    <el-button class="back-btn" @click="handleGoHome"
+      >{{ time }}秒后自动回首页</el-button
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "404",
+  data () {
+    return {
+      time: 5,
+      timer: null
+    }
+  },
+  mounted () {
+    this.autoGoHome()
+  },
+  methods: {
+    autoGoHome () {
+      if (this.time === 0) {
+        clearTimeout(this.timer)
+        this.$router.push('/')
+      } else {
+        this.timer = setTimeout(() => {
+          this.time--
+          this.autoGoHome()
+        }, 1000);
+      }
+    },
+    handleGoHome () {
+      clearTimeout(this.timer)
+      this.$router.push('/')
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.notFind {
+  img {
+    width: 100%;
+    height: 100vh;
+  }
+  .back-btn {
+    position: fixed;
+    left: 52%;
+    bottom: 29%;
+  }
+}
+</style>
